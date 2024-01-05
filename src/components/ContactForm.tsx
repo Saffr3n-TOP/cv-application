79e7ed587806ee06decb75a028f38346284bd09a
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-05 17:43:53 +0300

    Edit ContactForm component
    - Add defaultValue and required attributes to all form fields
    - Add form submit handler

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,18 +1,57 @@
-import type { GlobalProps, FormEvent } from '../types';
+import type { ContactProps, FormEvent } from '../types';
 import Form, { FormField } from './Form';
 import '../styles/form.sass';
 
-export default function ContactForm({ setEditMode }: GlobalProps) {
+export default function ContactForm({
+  setEditMode,
+  data,
+  setData
+}: ContactProps) {
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
-    setEditMode((prevState) => ({ ...prevState, contact: false }));
+
+    const form = e.target as HTMLFormElement;
+    const [name, email, phone] = [
+      ...(form.querySelectorAll('.input') as NodeListOf<HTMLInputElement>)
+    ].map((input) => input.value);
+
+    if (!form.checkValidity()) {
+      form.reportValidity();
+    } else {
+      setData!((prevState) => ({
+        ...prevState,
+        contact: { name, email, phone }
+      }));
+
+      setEditMode((prevState) => ({ ...prevState, contact: false }));
+    }
   };
 
   return (
     <Form onSubmit={onSubmit}>
-      <FormField id="name" label="Full Name" type="text" />
-      <FormField id="email" label="Email" type="email" />
-      <FormField id="phone" label="Phone Number" type="tel" />
+      <FormField
+        id="name"
+        label="Full Name"
+        defaultValue={data.name}
+        type="text"
+        required
+      />
+
+      <FormField
+        id="email"
+        label="Email"
+        defaultValue={data.email}
+        type="email"
+        required
+      />
+
+      <FormField
+        id="phone"
+        label="Phone Number"
+        defaultValue={data.phone}
+        type="tel"
+        required
+      />
     </Form>
   );
 }
