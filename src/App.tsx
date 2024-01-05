682476946a5129ede0d3fc9277b5003e1dc3985d
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-05 17:46:03 +0300

    Add data state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
+import type { EditMode, Data } from './types';
 import { useState } from 'react';
 import ContactForm from './components/ContactForm';
 import ContactSection from './components/ContactSection';
 import './styles/app.sass';
 
 export default function App() {
-  const [editMode, setEditMode] = useState({
+  const [editMode, setEditMode] = useState<EditMode>({
     contact: true
+  });
+
+  const [data, setData] = useState<Data>({
+    contact: {
+      name: '',
+      email: '',
+      phone: ''
+    }
   });
 
   return (
     <>
       {editMode.contact ? (
-        <ContactForm setEditMode={setEditMode} />
+        <ContactForm
+          setEditMode={setEditMode}
+          data={data.contact}
+          setData={setData}
+        />
       ) : (
-        <ContactSection setEditMode={setEditMode} />
+        <ContactSection setEditMode={setEditMode} data={data.contact} />
       )}
     </>
   );
