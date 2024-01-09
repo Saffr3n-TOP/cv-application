443b1ee5f8a0d6296803db3e4a7f0a3c3e55a552
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-09 21:23:46 +0300

    Create SectionDatesRange component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -12,6 +12,12 @@ type SectionItemProps = {
   data: string;
 };
 
+type SectionDatesRangeProps = {
+  label: string;
+  start: string;
+  end: string;
+};
+
 export default function Section({ children, onEdit }: SectionProps) {
   return (
     <dl className="section">
@@ -27,5 +33,18 @@ export function SectionItem({ label, data }: SectionItemProps) {
       <dt className="section__label">{label}</dt>
       <dd className="section__data">{data}</dd>
     </li>
+  );
+}
+
+export function SectionDatesRange({
+  label,
+  start,
+  end
+}: SectionDatesRangeProps) {
+  return (
+    <div className="section__dates">
+      <SectionItem label={`${label} Start`} data={start} />
+      <SectionItem label={`${label} End`} data={end || 'Present'} />
+    </div>
   );
 }
